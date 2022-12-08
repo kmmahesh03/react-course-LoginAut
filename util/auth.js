e77b7c77a2e20d7332077680dbde9bf58f027433
kmmahesh03 <57348583+kmmahesh03@users.noreply.github.com>
2022-12-08 12:45:39 +0530

    Add files via upload

---
+++ util/auth.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_KEY='AIzaSyDbJN8tGPsK9M9-VV_rCtRBkN-ho8J9HEg';
+
+async function authenticate(mode,email,password)
+{
+    const url=`https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+        const response=await axios.post(url,{
+        email:email,
+        password:password,
+        returnSecureToken:true,         
+    });
+
+    const token=response.data.idToken;
+    return token;
+}
+
+export  function createUser(email,password)
+{
+    return authenticate('signUp',email,password);
+    
+    
+}
+
+export function login(email,password)
+{ 
+    return authenticate('signInWithPassword',email,password);
+   
+}
